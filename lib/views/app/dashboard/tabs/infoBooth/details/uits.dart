674f61dfb50da743382bfeaa3881a62d0b1c39fc
Afsana <tarinakter567@gmail.com>
2023-07-08 15:36:30 +0600

    this is my UITS commit.

--- lib/views/app/dashboard/tabs/infoBooth/details/uits.dart
+++ lib/views/app/dashboard/tabs/infoBooth/details/uits.dart
@@ -16,7 +16,13 @@ class _Uits extends State<Uits>{
       ),
       body: Container(
         padding: EdgeInsets.all(10),
-        child: Text("Write here university details")
+        child: Text("The University of Information Technology and Sciences (UITS), "
+            "the first IT-based private university in Bangladesh, was founded on August 7, 2003, as a non-profit organization,"
+            "The vice chancellor of the university is the chief executive. Each academic department has its own head. On top of the administration,"
+            " there is the Board of Trustees (BoT), headed by a chairperson, which oversees policy matters. "
+            "In the academic sphere, the highest authority is vested in the Academic Council represented by heads of schools and teaching departments,"
+            " representatives of teachers and nominated members who are associated with education, science, culture, industries, media and scientific and cultural organizations in the country. "
+            "The council is presided over by the vice chancellor. The registrar of the university acts as the secretary who is also the custodian of the university seal.")
       )
     );
   }
